3a206e2e9b0189be19098a5277b5a686d14fd97b
sid2487 <sid454804@gmail.com>
2025-11-23 08:49:12 +0530

    added socket logic

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -71,7 +71,8 @@ wss.on("connection", (socket, request) => {
             socket.send(JSON.stringify({
                 type: "room-not-found",
                 roomId
-            }))
+            }));
+            return;
         };
 
 
@@ -99,10 +100,120 @@ wss.on("connection", (socket, request) => {
             roomId
         }));
 
+    };
+
+    if(parsedMessage.type === "chat"){
+      const roomId = parsedMessage.roomId;
+      const content = parsedMessage.content;
+      const wsUser = users.find(u => u.socket === socket);
+
+      if(!wsUser) return;
+
+      const roomExist = await prisma.room.findUnique({
+        where: {
+          id: roomId,
+        }
+      });
+      if(!roomExist){
+        socket.send(JSON.stringify({
+          type: "error",
+          message: "Room does not Exist"
+        }));
+        return;
+      };
+
+      const isMember = await prisma.roomMember.findFirst({
+        where: {
+          roomId,
+          userId: wsUser.userId,
+        }
+      });
+
+      if(!isMember){
+        socket.send(JSON.stringify({
+          type: "error",
+          message: "You cannot send Messages to this room",
+        }))
+        return;
+      };
+
+      if(!content || content.trim() === "") return;
+
+      const savedMessage = await prisma.message.create({
+        data: {
+          roomId,
+          userId: wsUser.userId,
+          content
+        },
+        include: { user: true }
+      });
+
+      users.forEach(u => {
+        if(u.rooms.includes(roomId.toString())){
+          u.socket.send(JSON.stringify({
+            type: "new-message",
+            message: savedMessage
+          }))
+        }
+      })
+
+    };
+
+    if(parsedMessage.type === "leave"){
+      const wsUser = users.find(u => u.socket === socket);
+      const roomId = parsedMessage.roomId;
+      if(!wsUser) return;
+
+      const roomExist = await prisma.room.findUnique({
+        where: {
+          id: roomId
+        }
+      });
+
+      if(!roomExist){
+        socket.send(JSON.stringify({
+          type: "room-not-found",
+          message: "Room can not Found",
+        }));
+        return;
+      };
+
+      const isMember = await prisma.roomMember.findFirst({
+        where: {
+          roomId,
+          userId: wsUser.userId,
+        }
+      });
+
+      if(!isMember){
+        socket.send(JSON.stringify({
+          type: "error",
+          message: "You cannot Leave this room",
+        }));
+        return;
+      };
+
+      wsUser.rooms = wsUser.rooms.filter(u => u !== parsedMessage.roomId.toString() )
+
+      socket.send(
+        JSON.stringify({
+          type: "left",
+          roomId,
+        })
+      );
+
     }
 
     
   })
+
+  socket.on("close", () => {
+    const index = users.findIndex((u) => u.socket === socket);
+    if (index !== -1) {
+      users.splice(index, 1);
+    }
+  });
+
 
   
 });
